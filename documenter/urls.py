f15bbb76924a0aab76bb13abcbfb9182552e02bd
bjgoode <bjgoode@vt.edu>
2019-09-30 08:57:25 -0400

    update urls for new django

--- documenter/urls.py
+++ documenter/urls.py
@@ -16,13 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include, re_path
 from django.views.generic import RedirectView
-from django.contrib.auth.views import logout, login
+from django.contrib.auth.views import LogoutView, LoginView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('rate_doc/', include('rate_doc.urls')),
-    path('logout/', logout, {'next_page': '/'}, name='logout'),
-    path('login/', login, name='login'),
+    path('logout/', LogoutView.as_view(template_name='users/logout.html'), {'next_page': '/'}, name='logout'),
+    path('login/', LoginView.as_view(template_name='users/login.html'), name='login'),
     path('', RedirectView.as_view(pattern_name='get-next', permanent=False)),
 ]
